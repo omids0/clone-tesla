f7e42e91308f06d4ea8a3e7ce70b43ec63440466
omids0 <soheilniao@gmail.com>
2022-01-28 23:23:41 +0330

    footer-added

--- src/components/Container.js
+++ src/components/Container.js
@@ -62,5 +62,6 @@ export default Container;
 
 const Content = styled.div`
             height: 100vh;
+            width: 100%
 `
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Footer() {
+      const [year, setYear] = useState('')
+      const time = new Date()
+
+      useEffect(() =>{
+            setYear(time.getFullYear())
+      },[])
+
       return <div className="footer">
-            footer
+            <ul>
+                  <li>Tesla @ {year}</li>
+                  <li>Privacy & Legal</li>
+                  <li>Contact</li>
+                  <li>Careers</li>
+                  <li>News</li>
+                  <li>Engage</li>
+                  <li>Locations</li>
+            </ul>
       </div>;
 }
 
